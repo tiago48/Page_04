ee62a0d4aa769d446842a94e8a23c7de316bfec0
tiago48 <31600397+tiago48@users.noreply.github.com>
2020-09-29 20:06:39 -0300

    countdown

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function revealMessage () {
   document.getElementById('hiddenMessage').style.display = 'block'
 }
+
+function countDown () {
+  let currentValue = document.getElementById('countDownButton').innerHTML
+  let newValue = 0
+  if (currentValue > 0) {
+    newValue = currentValue - 1
+  }
+  document.getElementById('countDownButton').innerHTML = newValue
+}
